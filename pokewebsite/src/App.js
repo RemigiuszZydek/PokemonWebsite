ac97c41389716ab3834b3cf639a45790e3d18a12
Remik <zyduremik@gmail.com>
2024-03-05 09:25:30 +0100

    change folders and add router

--- pokewebsite/src/App.js
+++ pokewebsite/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import Pokemon from "./Pokemon.js";
+import SearchOneSite from "./SearchOneSite.js";
 function App() {
 	return (
 		<div className="App">
-			<Pokemon />
+			<SearchOneSite />
 		</div>
 	);
 }
---
+++ pokewebsite/src/SearchOneSite.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Pokemon from "./searchPokemons/Pokemon";
+
+function SearchOneSite() {
+	return <Pokemon />;
+}
+export default SearchOneSite;
