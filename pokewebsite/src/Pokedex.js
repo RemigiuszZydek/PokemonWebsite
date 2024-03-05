ce7a951d96f94d4306e42e72f691a5ff26714559
Remik <zyduremik@gmail.com>
2024-03-05 10:15:50 +0100

    add other gens

--- pokewebsite/src/Pokedex.js
+++ pokewebsite/src/Pokedex.js
@@ -1,24 +1,63 @@
-// import { useState, useEffect } from "react";
-// import axios from "axios";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Pokedex() {
-	// const [pokemon, setPokemon] = useState(null);
-	// useEffect(() => {
-	// 	axios
-	// 		.get("https://pokeapi.co/api/v2/pokemon/pikachu")
-	// 		.then((response) => {
-	// 			console.log(response.data); // Dodaj to, aby zobaczyć odpowiedź
-	// 			setPokemon(response.data);
-	// 			console.log(pokemon);
-	// 		})
-	// 		.catch((error) => console.error("There was an error!", error));
-	// }, []);
-	// return (
-	// 	<div>
-	// 		<h1>{pokemon.name}</h1>
-	// 		<img src={pokemon.sprites.front_default} alt={pokemon.name} />
-	// 	</div>
-	// );
+	const [pokemonList, setPokemonList] = useState([]);
+	const [generation, setGeneration] = useState(1); // Domyślnie ustawiona na pierwszą generację
+
+	useEffect(() => {
+		axios
+			.get(`https://pokeapi.co/api/v2/generation/${generation}/`)
+			.then((response) => {
+				const promises = response.data.pokemon_species.map((species) =>
+					axios
+						.get(`https://pokeapi.co/api/v2/pokemon/${species.name}`)
+						.catch((error) => {
+							console.error(`Nie znaleziono Pokémona: ${species.name}`, error);
+							return null; // Zwracamy null, aby łatwo było to odfiltrować później
+						})
+				);
+
+				Promise.all(promises).then((pokemonDetailsResponses) => {
+					const pokemonDetails = pokemonDetailsResponses
+						.filter((response) => response !== null) // Odfiltrowujemy nieudane próby
+						.map((response) => ({
+							name: response.data.name,
+							image: response.data.sprites.front_default,
+							types: response.data.types.map((typeInfo) => typeInfo.type.name),
+						}));
+
+					setPokemonList(pokemonDetails);
+				});
+			})
+			.catch((error) => console.error("Jest błąd", error));
+	}, [generation]);
+
+	return (
+		<div>
+			<h1>Pokédex - Generacja {generation}</h1>
+			<select
+				value={generation}
+				onChange={(e) => setGeneration(e.target.value)}
+			>
+				{/* Generowanie opcji dla różnych generacji */}
+				{[1, 2, 3, 4, 5, 6, 7, 8].map((gen) => (
+					<option key={gen} value={gen}>
+						Generacja {gen}
+					</option>
+				))}
+			</select>
+			<ul>
+				{pokemonList.map((pokemon) => (
+					<li key={pokemon.name}>
+						<h2>{pokemon.name}</h2>
+						<img src={pokemon.image} alt={pokemon.name} />
+						<p>Typy: {pokemon.types.join(", ")}</p>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default Pokedex;
