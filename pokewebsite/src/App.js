4daa4070540e1d46a634a3e3cd610629a546b48e
Remik <zyduremik@gmail.com>
2024-03-05 09:32:45 +0100

    add router

--- pokewebsite/src/App.js
+++ pokewebsite/src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
 import SearchOneSite from "./SearchOneSite.js";
+import Pokedex from "./Pokedex.js";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Pokemon from "./searchPokemons/Pokemon.js";
 function App() {
 	return (
-		<div className="App">
-			<SearchOneSite />
-		</div>
+		<Router>
+			<div className="App">
+				<nav>
+					<ul>
+						<li>
+							<Link to="/SearchOneSite">Find Pokemon</Link>
+						</li>
+						<li>
+							<Link to="/Pokedex">1st Gen Pokedex</Link>
+						</li>
+					</ul>
+				</nav>
+				<Routes>
+					<Route path="/SearchOneSite" element={<Pokemon />} />
+					<Route path="/Pokedex" element={<Pokedex />} />
+					<Route path="/" element={<Pokemon />} />
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
